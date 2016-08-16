1cb9e29cfda1e100b935b61988d5f20406c6b3a6
hugh_tsai <shavenking@gmail.com>
2016-08-16 13:53:28 +0800

    新增 CreateMessageCommand 的測試

---
+++ message_board_symfony/tests/AppBundle/Command/CreateMessageCommandTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\AppBundle\Command;
+
+use AppBundle\Command\CreateMessageCommand;
+use AppBundle\Entity\Message;
+use AppBundle\Test\CommandTestCase;
+
+class CreateMessageCommandTest extends CommandTestCase
+{
+    protected $commandClass = CreateMessageCommand::class;
+
+    public function testCommandName()
+    {
+        $commandName = 'app:create-message';
+
+        $command = $this->application->find($commandName);
+
+        $this->assertSame(
+            get_class($command),
+            $this->commandClass
+        );
+    }
+
+    public function testCreateMessage()
+    {
+        $message = new Message(
+            'test_name_' . rand(),
+            'test_body_' . rand()
+        );
+
+        // run command
+        $this->execute([
+            'displayName' => $message->getDisplayName(),
+            'msgBody' => $message->getBody()
+        ]);
+
+        // see in database
+        $this->assertExistInDatabase('AppBundle:Message', [
+            'displayName' => $message->getDisplayName(),
+            'body' => $message->getBody()
+        ]);
+    }
+}
