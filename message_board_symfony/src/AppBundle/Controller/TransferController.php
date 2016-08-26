228cc5cee465336acdcaccbd9dddeb4bdb7606a7
hugh_tsai <shavenking@gmail.com>
2016-08-26 17:26:57 +0800

    重構 TransferController

--- message_board_symfony/src/AppBundle/Controller/TransferController.php
+++ message_board_symfony/src/AppBundle/Controller/TransferController.php
@@ -16,19 +16,20 @@ class TransferController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $em = $this->getDoctrine()->getManager();
-        $repository = $em->getRepository('AppBundle:Transfer');
-
         $username = $this->getUser()->getUsername();
         $page = $request->get('page', 1);
         $limit = $request->get('limit', 10);
-        $transfers = $repository->getPaginator($username, $page, $limit);
 
-        $viewPath = 'AppBundle:Transfer:index.html.twig';
+        $transfers = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('AppBundle:Transfer')
+            ->getPaginator($username, $page, $limit);
+
+        // pagination metadata
         $firstPage = 1;
         $lastPage = ceil($transfers->count() / $limit);
-        $renderedData = compact('transfers', 'firstPage', 'lastPage', 'limit');
 
+        // response json data if requested
         if ($request->headers->contains('content-type', 'application/json')) {
             return $this->json([
                 'data' => $transfers->getIterator(),
@@ -41,7 +42,10 @@ class TransferController extends Controller
             ]);
         }
 
-        return $this->render($viewPath, $renderedData);
+        $viewPath = 'AppBundle:Transfer:index.html.twig';
+        $viewData = compact('transfers', 'firstPage', 'lastPage', 'limit');
+
+        return $this->render($viewPath, $viewData);
     }
 
     /**
@@ -53,44 +57,12 @@ class TransferController extends Controller
      */
     public function storeDepositAction(Request $request)
     {
-        $data = json_decode($request->getContent(), true);
-
-        if (is_null($data)) {
-            throw new HttpException(400);
-        }
-
-        $amount = $data['amount'];
-
-        // validate amount
-        // in deposit action, amount has to be positive
-        if ($amount <= 0) {
-            throw new HttpException(403, 'Amount has to be positve.');
-        }
-
+        $amount = $this->getAmountFromRequest($request);
         $user = $this->getUser();
-        $em = $this->getDoctrine()->getManager();
-        $transfer = new Transfer($amount);
-
-        $em->transactional(function ($em) use ($user, $amount, $transfer) {
-            $repository = $em->getRepository('AppBundle:Transfer');
-            $latestBalance = $repository->getLatestBalance($user, $lock = true);
-
-            $transfer->setUser($user);
-            $transfer->setBalance($latestBalance + $amount);
-
-            $em->persist($transfer);
-        });
-
-        $data = [
-            'transfer' => [
-                'id' => $transfer->getId(),
-                'amount' => $transfer->getAmount(),
-                'transfered_at' => $transfer->getTransferedAt()
-            ]
-        ];
+        $transfer = $this->persistTransfer($user, $amount);
 
         return $this->json([
-            'data' => $data
+            'data' => $transfer
         ]);
     }
 
@@ -103,45 +75,56 @@ class TransferController extends Controller
      */
     public function storeWithdrawalAction(Request $request)
     {
+        $amount = $this->getAmountFromRequest($request) * -1;
+        $user = $this->getUser();
+        $transfer = $this->persistTransfer($user, $amount);
+
+        return $this->json([
+            'data' => $transfer
+        ]);
+    }
+
+    protected function getAmountFromRequest(Request $request)
+    {
         $data = json_decode($request->getContent(), true);
 
         if (is_null($data)) {
-            throw new HttpException(400);
+            throw new HttpException(400, 'Request content should be JSON.');
         }
 
         $amount = $data['amount'];
 
-        // validate amount
-        // in withdrawal action, amount has to be negative
         if ($amount <= 0) {
             throw new HttpException(403, 'Amount has to be positive.');
         }
 
-        $user = $this->getUser();
-        $em = $this->getDoctrine()->getManager();
-        $amount *= -1;
-        $transfer = new Transfer($amount);
+        return $amount;
+    }
 
-        $em->transactional(function ($em) use ($user, $amount, $transfer) {
-            $repository = $em->getRepository('AppBundle:Transfer');
+    protected function persistTransfer($user, $amount)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('AppBundle:Transfer');
+
+        $transfer = new Transfer;
+
+        // start transactional and
+        // lock the latest balance
+        $em->transactional(function ($em) use (
+            $user,
+            $amount,
+            $transfer,
+            $repository
+        ) {
             $latestBalance = $repository->getLatestBalance($user, $lock = true);
 
+            $transfer->setAmount($amount);
             $transfer->setUser($user);
             $transfer->setBalance($latestBalance + $amount);
 
             $em->persist($transfer);
         });
 
-        $data = [
-            'transfer' => [
-                'id' => $transfer->getId(),
-                'amount' => $transfer->getAmount(),
-                'transfered_at' => $transfer->getTransferedAt()
-            ]
-        ];
-
-        return $this->json([
-            'data' => $data
-        ]);
+        return $transfer;
     }
 }
