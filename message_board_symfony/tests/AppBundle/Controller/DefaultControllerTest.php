aa38019ef61c8b6920a19671f9681f395ac72fff
hugh_tsai <shavenking@gmail.com>
2016-08-30 17:04:52 +0800

    修正 DefaultControllerTest

--- message_board_symfony/tests/AppBundle/Controller/DefaultControllerTest.php
+++ message_board_symfony/tests/AppBundle/Controller/DefaultControllerTest.php
@@ -2,17 +2,29 @@
 
 namespace Tests\AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use AppBundle\DataFixtures\ORM\LoadUserData;
+
+use Liip\FunctionalTestBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+    protected function setUp()
+    {
+        $this->loadFixtures([LoadUserData::class]);
+    }
+
     public function testIndex()
     {
-        $client = static::createClient();
+        $client = static::makeClient($this->getCredentials());
 
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+    }
+
+    protected function getCredentials()
+    {
+        return ['username' => 'hugh', 'password' => 'hugh'];
     }
 }
